3bc9f1b4d34b4297a8d08ce694db4056878b1472
Jon Sweeney <jonsweeney26@gmail.com>
2021-11-24 09:29:11 -0800

    Initial Commit

---
+++ SweenBank/SweenBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweenBankApp.swift
+//  SweenBank
+//
+//  Created by Jonathan Sweeney on 11/24/21.
+//
+
+import SwiftUI
+
+@main
+struct SweenBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
